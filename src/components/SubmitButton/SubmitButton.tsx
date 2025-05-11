79cc1825c9b74c9d91c744ae2cd4fa472bdf1e4a
Anthony Radose <asradose@gmail.com>
2025-05-11 11:26:49 +0100

    Add initial styling for SubmitButton component

--- src/components/SubmitButton/SubmitButton.tsx
+++ src/components/SubmitButton/SubmitButton.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import type { SubmitButtonProps } from "../../types";
+import styles from "./SubmitButton.module.css";
 
 const SubmitButton: React.FC<SubmitButtonProps> = ({ onSubmit }) => {
-    return (
-      <button type="button" onClick={onSubmit} aria-label="Submit selected options">
-        Submit
-      </button>
-    );
-  };
-  
+  return (
+    <button
+      type="button"
+      onClick={onSubmit}
+      className={styles["submit-button"]}
+      aria-label="Submit selected options"
+    >
+      Submit
+    </button>
+  );
+};
 
 export default SubmitButton;
