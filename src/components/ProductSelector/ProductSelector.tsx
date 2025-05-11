6e23b304b0417e07833f275ea765f7257de3af46
Anthony Radose <asradose@gmail.com>
2025-05-11 11:08:46 +0100

    Add initial styling for ProductSelector component

--- src/components/ProductSelector/ProductSelector.tsx
+++ src/components/ProductSelector/ProductSelector.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import type { ProductSelectorProps } from "../../types";
-
+import styles from "./ProductSelector.module.css";
 
 const ProductSelector: React.FC<ProductSelectorProps> = ({ products, onProductChange }) => {
   return (
-    <div>
+    <div className={styles["product-selector-container"]}>
       <label htmlFor="product-select">Choose a product:</label>
-      <select id="product-select" onChange={(e) => onProductChange(e.target.value)}>
+      <select
+        id="product-select"
+        onChange={(e) => onProductChange(e.target.value)}
+      >
         {products.map((product) => (
           <option key={product.product} value={product.product}>
             {product.product}
